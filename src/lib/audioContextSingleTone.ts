079eb66d5fea554010039802c94fb00a7965a30c
JacoMig <jacomig@gmail.com>
2025-01-22 18:02:27 +0100

    fix: resume browser  audiocontext

--- src/lib/audioContextSingleTone.ts
+++ src/lib/audioContextSingleTone.ts
@@ -4,5 +4,8 @@ export function getAudioContext() {
   if (!audioContext) {
     audioContext = new AudioContext();
   }
+  if (audioContext.state === 'suspended') {
+    audioContext.resume();
+  }
   return audioContext;
 }
